660e05524dc5dc1756d033f14edf32dc1eaeb606
Rajat Sran <rajatsran27@gmail.com>
2021-06-12 20:13:35 +0530

    Minimum number of deletion in a string to make it a palindrome

---
+++ dynamic_programming/minDeletionTomakePalindrome.cpp
@@ -0,0 +1,56 @@
+// C++ program to print Minimum number of deletion in a string to make it a palindrome
+#include <bits/stdc++.h>
+using namespace std;
+
+int t[1002][1002];
+int LCS(string x, string y, int m, int n) {
+    //intialization
+    for (int i = 0; i < m + 1; ++i)
+    {
+        for (int j = 0; j < n + 1; ++j)
+        {
+            if (i == 0 || j == 0)
+            {
+                t[i][j] = 0;
+            }
+        }
+    }
+
+    //filling table according to lcs
+    for (int i = 1; i < m + 1; ++i)
+    {
+        for (int j = 1; j < n + 1; ++j)
+        {
+            if (x[i - 1] == y[j - 1])
+            {
+                t[i][j] = t[i - 1][j - 1] + 1;
+            } else {
+                t[i][j] = max(t[i - 1][j], t[i][j - 1]);
+            }
+        }
+    }
+    return t[m][n];
+
+}
+
+// Driver code
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    string x;
+    string y;
+    cin >> x;
+    y = x;
+    reverse(y.begin(), y.end());
+
+    int m = x.size(), n = y.size();
+    cout <<  m - LCS(x, y, m, n);
+
+    return 0;
+}
+
+
+
