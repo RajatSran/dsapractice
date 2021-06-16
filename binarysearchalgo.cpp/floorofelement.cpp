19a6f4558e7e87a76f169d616849fe9ff70850bb
Rajat Sran <rajatsran27@gmail.com>
2021-06-16 10:06:41 +0530

    find floor

---
+++ binarysearchalgo.cpp/floorofelement.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int floor(vector<int> x, int target) {
+    int i = 0;
+    int j = x.size();
+    int result = -1;
+    int mid;
+    while (i <= j) {
+        mid = (i + j) / 2;
+        if (x[mid] == target)
+        {
+            return x[mid];
+        } else if (x[mid] < target) {
+            result = x[mid];
+            i = mid + 1;
+        } else {
+            j = mid - 1;
+        }
+    }
+    return result;
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    vector<int> x = {  -1, 2, 10, 12 , 38};
+
+
+    cout << floor(x, 4);
+    return 0;
+}
+
