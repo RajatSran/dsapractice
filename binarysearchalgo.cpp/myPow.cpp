9a85ec37d8e83b0de5032e3efa72b1f17b6dca7f
Rajat Sran <rajatsran27@gmail.com>
2021-07-02 00:14:08 +0530

    mypow

---
+++ binarysearchalgo.cpp/myPow.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+double myPow(double x, int n) {
+	if (n == 0)
+		return 1;
+	double temp = myPow(x, abs(n / 2));
+	if (n < 0) {
+		if (n % 2 == 0)
+			return 1 / (temp * temp);
+		else
+			return 1 / (temp * temp * x);
+	}
+	else {
+		if (n % 2 == 0)
+			return (temp * temp);
+		else
+			return (temp * temp * x);
+	}
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	cout << myPow(12, 2);
+}
