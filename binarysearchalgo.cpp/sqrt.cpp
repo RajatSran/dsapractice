d7a8ce08c7128734ea23c55112fafe29c8393f2a
Rajat Sran <rajatsran27@gmail.com>
2021-07-02 04:17:46 +0530

    two types of sqrt

--- binarysearchalgo.cpp/sqrt.cpp
+++ binarysearchalgo.cpp/sqrt.cpp
@@ -1,26 +1,47 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int mySqrt(int x) {
-	if (x == 0)
-		return 0;
-	int left = 1, right = INT_MAX;
-	while (true) {
-		int mid = left + (right - left) / 2;
-		if (mid > x / mid) {
-			right = mid - 1;
+double eps = 1e-6; //10^(-6)
+
+//more precision more complexity
+double SQR(double x) {
+	if (x == 0) return 0;
+	double lo = 1, hi = x, mid;
+	while (hi - lo > eps) {
+		mid = (hi + lo) / 2;
+		if (mid * mid < x) {
+			lo = mid;
 		} else {
-			if (mid + 1 > x / (mid + 1))
-				return mid;
-			left = mid + 1;
+			hi = mid;
 		}
 	}
+	return lo;
 }
+
+int mySqrt(int x)
+{
+	if (x == 0) return 0;
+	int lo = 1, hi = x, mid;
+	while (hi - lo >= 0 ) {
+		mid = lo + (hi - lo) / 2;
+		if (mid  == x / mid) {
+			return mid;
+		}
+		else if (mid < x / mid) {
+			lo = mid + 1;
+		} else {
+			hi = mid - 1;
+		}
+	}
+	return lo - 1;
+}
+
 
 int main() {
 #ifndef ONLINE_JUDGE
 	freopen("input.txt", "r", stdin);
 	freopen("output.txt", "w", stdout);
 #endif
-	cout << mySqrt(144);
+	// cout << SQR(5) << endl;
+	cout << mySqrt(8) << endl;
 }
