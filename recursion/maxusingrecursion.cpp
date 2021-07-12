b35897a4d7f77dcadd301c52917c85f613496024
Rajat Sran <rajatsran27@gmail.com>
2021-07-12 18:04:13 +0530

    max using reccursiion

---
+++ recursion/maxusingrecursion.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int maxn = INT_MIN;
+int maxusingrec(vector<int> arr, int idx) {
+	if (idx < 0) {
+		return maxn;
+	}
+	maxn = maxusingrec(arr, idx - 1);
+	return maxn = max(maxn, arr[idx]);
+}
+int main() {
+// #ifndef ONLINE_JUDGE
+// 	freopen("input.txt", "r", stdin);
+// 	freopen("output.txt", "r", stdout);
+// #endif
+	vector<int> arr = {1, 2, 5, 3, 6, 9, 3};
+	cout << maxusingrec(arr, 6);
+
+}
