9ab957256e0877974044743d03e0fcd5ce31f223
Rajat Sran <rajatsran27@gmail.com>
2021-08-25 18:45:56 +0530

    optimise

--- arraypractice/countinversions.cpp
+++ arraypractice/countinversions.cpp
@@ -2,29 +2,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int _mergeSort(int arr[], int temp[], int left, int right);
-int merge(int arr[], int temp[], int left, int mid, int right);
 
-int mergeSort(int arr[], int array_size)
-{
-	int temp[array_size];
-	return _mergeSort(arr, temp, 0, array_size - 1);
-}
 
-int _mergeSort(int arr[], int temp[], int left, int right)
-{
-	int mid, inv_count = 0;
-	if (right > left) {
-
-		mid = (right + left) / 2;
-		inv_count += _mergeSort(arr, temp, left, mid);
-		inv_count += _mergeSort(arr, temp, mid + 1, right);
-
-		/*Merge the two parts*/
-		inv_count += merge(arr, temp, left, mid + 1, right);
-	}
-	return inv_count;
-}
 
 int merge(int arr[], int temp[], int left, int mid, int right)
 {
@@ -56,6 +35,21 @@ int merge(int arr[], int temp[], int left, int mid, int right)
 	return inv_count;
 }
 
+int _mergeSort(int arr[], int temp[], int left, int right)
+{
+	int mid, inv_count = 0;
+	if (right > left) {
+
+		mid = (right + left) / 2;
+		inv_count += _mergeSort(arr, temp, left, mid);
+		inv_count += _mergeSort(arr, temp, mid + 1, right);
+
+		/*Merge the two parts*/
+		inv_count += merge(arr, temp, left, mid + 1, right);
+	}
+	return inv_count;
+}
+
 int main()
 {
 #ifndef ONLINE_JUDGE
@@ -64,7 +58,8 @@ int main()
 #endif
 	int arr[] = { 1, 20, 6, 4, 5 };
 	int n = sizeof(arr) / sizeof(arr[0]);
-	int ans = mergeSort(arr, n);
+	int temp[n];
+	int ans = _mergeSort(arr, temp, 0, n - 1);
 	cout << " Number of inversions are " << ans;
 	return 0;
 }
